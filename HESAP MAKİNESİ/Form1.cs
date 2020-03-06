930cf1e7637d7ac9f8d3e5fbdefa407c1463ad60
F-tpr <61655548+F-tpr@users.noreply.github.com>
2020-03-06 22:59:07 +0300

    hesap makinesi yollama

---
+++ HESAP MAKİNESİ/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HESAP_MAKİNESİ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Sayı1, Sayı2, Sonuç;
+            Sayı1 = int.Parse(label1.Text);
+            Sayı2 = int.Parse(label2.Text);
+            Sonuç = Sayı1 + Sayı2;
+            label3.Text = Sonuç.ToString();
+
+        }
+    }
+}
